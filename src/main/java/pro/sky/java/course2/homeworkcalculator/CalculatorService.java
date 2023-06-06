f1f8b9442ed0408642b8653da0a5cc1bebf5b378
Aleksandr Katyushnyj <a-katushnii@yandex.ru>
2023-06-06 20:02:06 +0300

    Выполнил задание

---
+++ src/main/java/pro/sky/java/course2/homeworkcalculator/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homeworkcalculator;
+
+public interface CalculatorService {
+    String hello();
+
+    String plus(String firstValue, String secondValue);
+
+    String minus(String firstValue, String secondValue);
+
+    String multiply(String firstValue, String secondValue);
+
+    String divide(String firstValue, String secondValue);
+}
